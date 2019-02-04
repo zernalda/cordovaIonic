c5fe18969b0d72f9fca7d4889c992a0389a6c3d7
unknown <zernalda92@gmail.com>
2019-02-04 02:28:53 -0800

    test env baru

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'user', loadChildren: './user/user.module#UserPageModule' },
+  { path: 'user-list', loadChildren: './user-list/user-list.module#UserListPageModule' }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/tabs/tabs.module.ts
@@ -0,0 +1,23 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { TabsPageRoutingModule } from './tabs.router.module';
+
+import { TabsPage } from './tabs.page';
+
+import { UserPageModule } from "../user/user.module";
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    TabsPageRoutingModule,
+    UserPageModule
+  ],
+  declarations: [TabsPage]
+})
+export class TabsPageModule {}
