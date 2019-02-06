ef2cac713745d3c3d666131f21641254be9933d6
unknown <zernalda92@gmail.com>
2019-02-06 03:44:21 -0800

    learn config firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,16 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import * as firebase from 'firebase';
+
+
+const config = {
+  apiKey: 'AIzaSyChjgZ6sxyZ6H5XYW6zUA4SD1MHZsREK4s',
+  authDomain: 'ionicchat-4884d.firebaseapp.com',
+  databaseURL: 'ionicchat-4884d',
+  projectId: 'ionicchat-4884d.appspot.com',
+  storageBucket: 'ionicchat-4884d.appspot.com',
+};
 
 @Component({
   selector: 'app-root',
@@ -22,5 +32,6 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Router, ActivatedRoute } from '@angular/router';
+import * as firebase from 'Firebase';
+import { constructor } from 'Firebase';
+
+// infos = [];
+// ref = firebase.database().ref('infos/');
+
+// constructor(public router: Router, public loadingController: LoadingController) {
+//   this.ref.on('value', (resp: any) => {
+//     this.infos = [];
+//     this.infos = snapshotToArray(resp);
+//   });
+// }
+
+export const snapshotToArray = snapshot => {
+  let returnArr = [];
+
+  snapshot.forEach(childSnapshot => {
+    let item = childSnapshot.val();
+    item.key = childSnapshot.key;
+    returnArr.push(item);
+  });
+
+  return returnArr;
+};
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {}
+export class TabsPage { }
